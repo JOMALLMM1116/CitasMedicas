34e9ba6d9feaa7472dc4e230769fe2655f84312b
Manuel Leal <lealmanuel1611@gmail.com>
2026-02-23 14:49:55 -0600

    :label:Creacion de Modulos de Agendar Citas y Login de Secretaria

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ function searchPatient() {
     // Simular paciente encontrado — guardar en sessionStorage y redirigir
     errorEl.classList.add('hidden');
     sessionStorage.setItem('patientName', name);
-    window.location.href = 'pages/paciente-cita.html';
+    window.location.href = 'pages/pacienteCita.html';
 }
 
 // ===== PATIENT REGISTER =====
@@ -73,7 +73,7 @@ function registerPatient() {
 
     setTimeout(() => {
         sessionStorage.setItem('patientName', name);
-        window.location.href = 'pages/paciente-cita.html';
+        window.location.href = 'pages/pacienteCita.html';
     }, 1500);
 }
 
